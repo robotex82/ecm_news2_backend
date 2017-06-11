8c8edf1c8bfa682d9b9fb5408d37587845635daa
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-06-11 15:23:06 +0200

    Use concern instead of own implementation.

--- app/controllers/ecm/news/backend/items_controller.rb
+++ app/controllers/ecm/news/backend/items_controller.rb
@@ -1,17 +1,9 @@
 class Ecm::News::Backend::ItemsController < Itsf::Backend::Resource::BaseController
+  include Controller::FriendlyIdConcern
+  include Controller::ActsAsPublishedConcern
+
   def self.resource_class
     Ecm::News::Item
-  end
-
-  def toggle_published
-    @resource = resource_class.find(params[:id])
-    @resource.toggle_published!
-    action = @resource.published? ? :published : :unpublished
-    respond_with(
-      @resource,
-      location: collection_path,
-      notice: I18n.t("flash.actions.#{action_name}.#{action}.notice", inflections)
-    )
   end
 
   private
@@ -20,9 +12,5 @@ class Ecm::News::Backend::ItemsController < Itsf::Backend::Resource::BaseControl
     params
       .require(:item)
       .permit(:title, :locale, :body, :link_to_more, :published, pictures_attributes: [ :image, :_destroy, :id ])
-  end
-
-  def load_resource
-    load_scope.friendly.find(params[:id])
   end
 end
